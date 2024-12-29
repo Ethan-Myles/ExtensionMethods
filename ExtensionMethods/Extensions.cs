250c08743d3b170a2a325835c9d3291c1991bdb3
Ethan <78313955+Ethan-Myles@users.noreply.github.com>
2024-12-29 11:27:14 +0000

    Add project files.

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,40 @@
+﻿namespace ExtensionMethods{
+
+    public static class Extensions
+    {
+        public static bool IsLessThan(this int i, int value)
+        {
+            return i < value;
+        }
+
+        public static IEnumerable<List<T>> ChunkBy<T>(this List<T> list, int chunkSize)
+        {
+            //Check for valid chunk size
+            if (chunkSize <= 0)
+                throw new ArgumentException("Chunk size must be greater than zero.");
+
+            //Create chunks
+            for (int i = 0; i < list.Count; i += chunkSize)
+            {
+                yield return list.GetRange(i, Math.Min(chunkSize, list.Count - i));
+            }
+        }
+
+        public static IEnumerable<string> Capitalise(this IEnumerable<string> strings)
+        {
+            foreach (var str in strings)
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    yield return str;
+                }
+                else
+                {
+                    //Capitalise the first letter and make the rest lowercase
+                    yield return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+                }
+            }
+        }
+    }
+
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿using ExtensionMethods;
+
+class Program
+{
+    static void Main()
+    {
+        //First Extension method
+        var i = 101;
+        var isLessThan = i.IsLessThan(100);
+
+        //Console.WriteLine(isLessThan);
+
+        //Second extension method
+        var numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        var chunks = numbers.ChunkBy(3);
+
+        /*
+        foreach (var chunk in chunks)
+        {
+            Console.WriteLine(string.Join(", ", chunk));
+        }
+        */
+
+        //Third extension method
+
+        //var animals = new List<string> { "fish", "CAT", "dOg", "ELEphant", "Koala" };
+        //var animals = new HashSet<string> { "fish", "CAT", "dOg", "ELEphant", "Koala" };
+
+        string[] animals = { "fish", "CAT", "dOg", "ELEphant", "Koala" };
+
+        var capitalisedAnimals = animals.Capitalise();
+
+        foreach (var animal in capitalisedAnimals)
+        {
+            Console.WriteLine(animal);
+        }
+
+    }
+
+
+}
